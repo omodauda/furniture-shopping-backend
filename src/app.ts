cff8c7175e8521ed3b9c65bbd33b8485f938baeb
omodauda <omodauda.dl@gmail.com>
2022-08-21 12:50:46 +0100

    feat: Setup database connection

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,25 @@
 import express, { Express } from 'express';
+import db from './database/models'
+
+const { sequelize } = db;
 
 const app: Express = express();
+const ENV = process.env.NODE_ENV;
 
-app.use(express.json)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+async function initDb() {
+  if (ENV === 'development') {
+    try {
+      await sequelize.authenticate();
+      console.log('Database connection established');
+    } catch (error) {
+      console.error('Unable to connect to database:', error);
+    }
+  }
+}
+
+initDb()
 
 export default app;
