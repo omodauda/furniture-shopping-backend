39455f8c2a09cf7edc9e1bfe2a9e1d0d72b64e33
omodauda <omodauda.dl@gmail.com>
2022-08-26 20:30:38 +0100

    feat: User service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,11 +1,16 @@
-// import User from '../interfaces/user.interface';
+import { User, PrismaClient } from '@prisma/client';
 
-// // const { User: UserModel } = db;
 
-// export default class UserService {
-//   // private user = UserModel;
-//   static async createUser(email: string, password: string, full_name: string): Promise<User> {
-//     const response = await User.create({ email, password, full_name });
-//     return response;
-//   }
-// }
+export default class UserService {
+  public users = new PrismaClient().user;
+
+  public async createUser(email: string, password: string, fullName: string): Promise<User> {
+    return await this.users.create({
+      data: {
+        email,
+        password,
+        fullName
+      },
+    })
+  }
+}
