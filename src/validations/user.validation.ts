c31d8c83fe3a4f18c1d79c46f66232d9db4fd77b
omodauda <omodauda.dl@gmail.com>
2022-08-26 22:53:13 +0100

    feat: SignUp validation schema

---
+++ src/validations/user.validation.ts
@@ -0,0 +1,34 @@
+import Joi from 'joi';
+
+const signUpValidationSchema = Joi.object({
+  email: Joi.string()
+    .email({ minDomainSegments: 2 })
+    .min(5)
+    .max(100)
+    .required()
+    .messages({
+      'string.empty': 'email cannot be an empty field',
+      'string.email': 'please enter a valid email address',
+      'any.required': 'email is required'
+    }),
+  password: Joi.string()
+    .min(5)
+    .max(20)
+    .required()
+    .messages({
+      'string.empty': 'password cannot be an empty field',
+      'string.min': 'password should be minimum of 5 characters length',
+      'string.max': 'password should not be more than 20 characters length',
+      'any.required': 'password is required',
+    }),
+  fullName: Joi.string()
+    .min(5)
+    .required()
+    .messages({
+      'string.empty': 'full name cannot be an empty field',
+      'string.min': 'full name should be minimum of 5 characters length',
+      'any.required': 'full name is required'
+    }),
+});
+
+export { signUpValidationSchema }
