7525da0d7e76986c4817a52c4b12c08cee8fa255
omodauda <omodauda.dl@gmail.com>
2022-08-26 22:54:31 +0100

    feat: Check existing email in create user

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,10 +1,13 @@
 import { User, PrismaClient } from '@prisma/client';
+import HttpException from '../utils/handlers/error.handler';
 
 
 export default class UserService {
   public users = new PrismaClient().user;
 
   public async createUser(email: string, password: string, fullName: string): Promise<User> {
+    const existingEmail = await this.users.findUnique({ where: { email } });
+    if (existingEmail) throw new HttpException(409, `user with email ${email} already exists`)
     return await this.users.create({
       data: {
         email,
