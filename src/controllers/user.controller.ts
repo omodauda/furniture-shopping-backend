8cb55d11eff247091e1931c6236bdf26d36affab
omodauda <omodauda.dl@gmail.com>
2022-08-26 20:28:25 +0100

    feat: User signup

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,24 @@
-import { Request, Response, RequestHandler, NextFunction } from 'express';
-import HttpException from '../utils/handlers/error.handler';
-// import UserService from '../services/user.service';
+import { Request, Response, NextFunction } from 'express';
+import UserService from '../services/user.service';
 
 export default class UserController {
+
+  private UserService = new UserService();
+
+  public signUp = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
+    try {
+      const { email, password, fullName } = req.body;
+      await this.UserService.createUser(email, password, fullName);
+      return res
+        .status(201)
+        .json({
+          status: 'success',
+          message: 'user successfully registered'
+        })
+    } catch (error) {
+      next(error);
+    }
+  }
 
   // static async signUp(
   //   req: Request,
