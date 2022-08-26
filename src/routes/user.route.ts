17d79887a57485c8d93a91096b3861e1e2a5318e
omodauda <omodauda.dl@gmail.com>
2022-08-26 20:30:52 +0100

    feat: User route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,19 @@
-import express, { Router } from 'express';
+import { Router } from 'express';
 import UserController from '../controllers/user.controller';
+import Route from '../interfaces/routes.interface';
 
-const router: Router = express.Router();
+export default class UserRoute implements Route {
+  public path = '/user';
+  public router = Router();
+  public UserController = new UserController();
 
-// router
-//   .route('/signup')
-//   .post(UserController.signUp);
+  constructor() {
+    this.initializeRoutes();
+  }
 
-export default router;
+  private initializeRoutes() {
+    this.router
+      .route(`${this.path}/signup`)
+      .post(this.UserController.signUp);
+  }
+}
