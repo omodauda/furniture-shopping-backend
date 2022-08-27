5231d9e1f70c2aaf386fbc84345e24ecbbc2ed21
omodauda <omodauda.dl@gmail.com>
2022-08-27 16:43:05 +0100

    feat: Sign token util

---
+++ src/utils/token.ts
@@ -0,0 +1,12 @@
+import jwt from 'jsonwebtoken';
+import { User } from '@prisma/client';
+import { TokenPayload, TokenData } from '../interfaces/auth.interface';
+
+export const signToken = (user: User): TokenData => {
+  const payload: TokenPayload = { id: user.id };
+  const secretKey = process.env.JWT_SECRET;
+  const expiresIn: number = 60 * 60;
+
+  const token = jwt.sign(payload, secretKey as jwt.Secret, { expiresIn })
+  return { token, expiresIn }
+}
