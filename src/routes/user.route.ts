839371dc51a4a977e85222923bb148c4308c67ee
omodauda <omodauda.dl@gmail.com>
2022-08-28 12:45:55 +0100

    feat: Validate login request

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,7 +3,7 @@ import UserController from '../controllers/user.controller';
 import AddressController from '../controllers/address.controller';
 import Route from '../interfaces/routes.interface';
 import validationMiddleware from '../middlewares/validation.middleware';
-import { signUpValidationSchema } from '../validations/user.validation';
+import { signUpValidation, loginValidation } from '../validations/user.validation';
 import authMiddleware from '../middlewares/auth.middleware';
 
 export default class UserRoute implements Route {
@@ -19,11 +19,11 @@ export default class UserRoute implements Route {
   private initializeRoutes() {
     this.router
       .route(`${this.path}/signup`)
-      .post(validationMiddleware(signUpValidationSchema), this.UserController.signUp);
+      .post(validationMiddleware(signUpValidation), this.UserController.signUp);
 
     this.router
       .route(`${this.path}/login`)
-      .post(this.UserController.login);
+      .post(validationMiddleware(loginValidation), this.UserController.login);
 
     this.router
       .route(`${this.path}/address`)
--- src/validations/user.validation.ts
+++ src/validations/user.validation.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
-const signUpValidationSchema = Joi.object({
+const signUpValidation = Joi.object({
   email: Joi.string()
     .email({ minDomainSegments: 2 })
     .min(5)
@@ -31,4 +31,27 @@ const signUpValidationSchema = Joi.object({
     }),
 });
 
-export { signUpValidationSchema }
+const loginValidation = Joi.object({
+  email: Joi.string()
+    .email({ minDomainSegments: 2 })
+    .min(5)
+    .max(100)
+    .required()
+    .messages({
+      'string.empty': 'email cannot be an empty field',
+      'string.email': 'please enter a valid email address',
+      'any.required': 'email is required'
+    }),
+  password: Joi.string()
+    .min(5)
+    .max(20)
+    .required()
+    .messages({
+      'string.empty': 'password cannot be an empty field',
+      'string.min': 'password should be minimum of 5 characters length',
+      'string.max': 'password should not be more than 20 characters length',
+      'any.required': 'password is required',
+    }),
+})
+
+export { signUpValidation, loginValidation }
