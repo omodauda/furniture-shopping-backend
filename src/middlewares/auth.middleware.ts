4372825195de3ca08e9e5b2389c0aa9b8925949f
omodauda <omodauda.dl@gmail.com>
2022-08-28 12:38:45 +0100

    feat: Auth middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,31 @@
+import { NextFunction, Response, Request } from 'express';
+import jwt from 'jsonwebtoken';
+import { PrismaClient } from '@prisma/client';
+import HttpException from '../utils/handlers/error.handler';
+import { TokenPayload, AuthRequest } from '../interfaces/auth.interface';
+
+async function authMiddleware(req: AuthRequest, res: Response, next: NextFunction) {
+  const authorization = req.headers.authorization;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new HttpException(401, 'Unauthorized'));
+  }
+  const token = authorization.split('Bearer ')[1].trim();
+
+  try {
+    const { id: userId } = (jwt.verify(token, process.env.JWT_SECRET as jwt.Secret)) as TokenPayload;
+
+    const users = new PrismaClient().user;
+    const user = await users.findUnique({ where: { id: userId } })
+    if (!user) {
+      return next(new HttpException(401, 'Unauthorized'))
+    }
+    req.user = user;
+    return next();
+  } catch (error: any) {
+    return next(new HttpException(401, error.message))
+  }
+
+}
+
+export default authMiddleware;
