dae15facb54c28706d396ce16d4360c2729b2f5f
omodauda <omodauda.dl@gmail.com>
2022-09-01 09:58:40 +0100

    feat: Product route

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import dotenv from 'dotenv';
 import App from './app';
 
 import UserRoute from './routes/user.route';
+import ProductRoute from './routes/product.route';
 
 dotenv.config();
 
-const app = new App([new UserRoute()]);
+const app = new App([new UserRoute(), new ProductRoute()]);
 
 app.listen();
---
+++ src/routes/product.route.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import ProductController from '../controllers/product.controller';
+import Route from '../interfaces/routes.interface';
+import validationMiddleware from '../middlewares/validation.middleware';
+import { addProductValidation } from '../validations/product.validation';
+import { adminOnlyMiddleware, authMiddleware } from '../middlewares/auth.middleware';
+
+export default class ProductRoute implements Route {
+  public path = '/product';
+  public router = Router();
+  private ProductController = new ProductController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router
+      .route(`${this.path}`)
+      .post(
+        // validationMiddleware(addProductValidation),
+        authMiddleware,
+        adminOnlyMiddleware,
+        this.ProductController.addProduct
+      );
+  }
+}
