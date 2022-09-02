8e9d0836279dd79fa325294cdb41cfa9222f9abf
omodauda <omodauda.dl@gmail.com>
2022-09-02 13:15:59 +0100

    chore: Cloudinary upload util

---
+++ src/utils/cloudinary.ts
@@ -0,0 +1,23 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.cloudinary_api_key,
+  api_secret: process.env.cloudinary_api_secret
+});
+
+class Cloudinary {
+  private cloud = cloudinary;
+
+  public uploadImage = async (image: { path: string }, folder: string): Promise<object> => {
+    return await this.cloud.uploader.upload(image.path, {
+      folder: `furniture-shopping/${folder}`
+    })
+  }
+
+  public deleteImage = async (publicId: string): Promise<void> => {
+    await cloudinary.uploader.destroy(publicId)
+  }
+};
+
+export default Cloudinary
