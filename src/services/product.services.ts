08733604f0a1011185803d6b00bbcfab26107b08
omodauda <omodauda.dl@gmail.com>
2022-09-04 15:40:08 +0100

    feat: Create Product service

--- src/services/product.services.ts
+++ src/services/product.services.ts
@@ -1,16 +1,42 @@
-import { PrismaClient, Product } from '@prisma/client';
-import { ProductData } from '../interfaces/product.interface';
+import { PrismaClient, Product, ProductCategory } from '@prisma/client';
+import { ProductImage } from '../interfaces/product.interface';
 
 
 export default class ProductService {
 
   public product = new PrismaClient().product;
+  public category = new PrismaClient().productCategory;
+  public productImage = new PrismaClient().productImage;
 
-  public createProduct = async (product: ProductData): Promise<Product> => {
-    return await this.product.create({
+  public createProduct = async (
+    name: string,
+    description: string,
+    price: number,
+    quantity: number,
+    productCategoryId: string,
+    imageData: ProductImage[]
+  ): Promise<void> => {
+    const product = await this.product.create({
       data: {
-        ...product
+        name,
+        description,
+        price,
+        quantity,
+        productCategoryId
       }
     })
+    for (const image of imageData) {
+      await this.productImage.create({
+        data: {
+          productId: product.id,
+          url: image.url,
+          publicId: image.publicId
+        }
+      })
+    }
+  }
+
+  public getCategoryNameById = async (id: string): Promise<ProductCategory | null> => {
+    return await this.category.findUnique({ where: { id } });
   }
 }
