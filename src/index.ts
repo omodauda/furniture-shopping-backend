978f13017b504d84191e1e019a7bea582fad8990
omodauda <omodauda.dl@gmail.com>
2022-09-08 19:00:35 +0100

    feat: Instantiate cart route in app

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,15 @@ import App from './app';
 import UserRoute from './routes/user.route';
 import ProductRoute from './routes/product.route';
 import OrderRoute from './routes/order.route';
+import CartRoute from './routes/cart.route';
 
 dotenv.config();
 
 const app = new App([
   new UserRoute(),
   new ProductRoute(),
-  new OrderRoute()
+  new OrderRoute(),
+  new CartRoute()
 ]);
 
 app.listen();
