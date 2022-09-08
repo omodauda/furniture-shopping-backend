c4a0eb599034c0c458a30a474e7021f16f3590bb
omodauda <omodauda.dl@gmail.com>
2022-09-08 20:10:02 +0100

    feat: Get user cart service

--- src/services/cart.service.ts
+++ src/services/cart.service.ts
@@ -4,13 +4,33 @@ import prisma from '../lib/prisma';
 export default class CartService {
   public cartItem = prisma.cartItem;
 
-  public addToCart = async (userId: string, cartItemData: CartItem) => {
+  public addToCart = async (userId: string, cartItemData: CartItem): Promise<CartItem> => {
     return await this.cartItem.create({
       data: {
         userId,
         productId: cartItemData.productId,
         quantity: cartItemData.quantity
       }
+    })
+  }
+
+  public getUserCart = async (userId: string) => {
+    return await this.cartItem.findMany({
+      where: {
+        userId,
+      },
+      select: {
+        product: {
+          select: {
+            id: true,
+            name: true,
+            description: true,
+            images: true,
+            price: true,
+            quantity: true,
+          }
+        }
+      },
     })
   }
 }
