bda7103dc990b21e9aafab39ec74629c41d73944
omodauda <omodauda.dl@gmail.com>
2022-09-12 18:09:54 +0100

    feat: Update create order service

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,15 +1,30 @@
+import HttpException from '../utils/handlers/error.handler';
 import { Order } from '../interfaces/order.interface';
 import prisma from '../lib/prisma';
 
 export default class OrderService {
   public order = prisma.order;
   public orderItem = prisma.orderItem;
+  public userAddress = prisma.userAddress;
   public prisma = prisma;
 
   public createOrder = async (userId: string, orderData: Order): Promise<void> => {
+    const validUserAddress = await this.userAddress.findUnique({
+      where: {
+        id: orderData.deliveryAddressId,
+      }
+    });
+    if (!validUserAddress) {
+      throw new HttpException(400, 'invalid user address')
+    }
+    if (validUserAddress.userId !== userId) {
+      throw new HttpException(401, 'address does not belong to this user');
+    }
+
     const orderRecord = await this.order.create({
       data: {
         userId,
+        deliveryAddressId: orderData.deliveryAddressId,
         total: orderData.total,
       }
     });
