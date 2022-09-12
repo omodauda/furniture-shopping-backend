c411dc3f707e7bc6674915cda25cffb60e8ecd4c
omodauda <omodauda.dl@gmail.com>
2022-09-12 11:07:36 +0100

    feat: Remove cart item controller

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -2,6 +2,7 @@ import { Response, NextFunction } from 'express';
 import { AuthRequest } from '../interfaces/auth.interface';
 import CartService from '../services/cart.service';
 import { CartItem } from '../interfaces/cart.item.interface';
+import HttpException from '../utils/handlers/error.handler';
 
 
 export default class CartController {
@@ -40,6 +41,30 @@ export default class CartController {
             total,
             cart
           }
+        })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  public removeFromCart = async (req: AuthRequest, res: Response, next: NextFunction) => {
+    const { id: userId } = req.user;
+    const { id: cartItemId } = req.params
+    try {
+      const item = await this.CartService.getCartItem(cartItemId);
+      if (!item) {
+        throw new HttpException(400, 'cart item not found')
+      }
+
+      if (item.userId !== userId) {
+        throw new HttpException(401, 'Unauthorized')
+      }
+      await this.CartService.removeFromCart(cartItemId);
+      return res
+        .status(200)
+        .json({
+          status: 'success',
+          message: 'cart item removed successfully'
         })
     } catch (error) {
       next(error)
