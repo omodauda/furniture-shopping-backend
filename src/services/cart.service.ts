7e3231c1f94c3193608091be5e15bb6668da1a12
omodauda <omodauda.dl@gmail.com>
2022-09-12 11:07:01 +0100

    chore: Remove cart item service

--- src/services/cart.service.ts
+++ src/services/cart.service.ts
@@ -1,10 +1,20 @@
 import { CartItem } from '../interfaces/cart.item.interface';
 import prisma from '../lib/prisma';
+import HttpException from '../utils/handlers/error.handler';
 
 export default class CartService {
   public cartItem = prisma.cartItem;
 
   public addToCart = async (userId: string, cartItemData: CartItem): Promise<CartItem> => {
+    const existingCartItem = await this.cartItem.findFirst({
+      where: {
+        userId,
+        productId: cartItemData.productId,
+      }
+    });
+    if (existingCartItem) {
+      throw new HttpException(400, 'item already in cart')
+    }
     return await this.cartItem.create({
       data: {
         userId,
@@ -33,6 +43,22 @@ export default class CartService {
           }
         }
       },
+    })
+  }
+
+  public getCartItem = async (id: string): Promise<CartItem | null> => {
+    return await this.cartItem.findUnique({
+      where: {
+        id,
+      }
+    })
+  }
+
+  public removeFromCart = async (id: string): Promise<CartItem> => {
+    return await this.cartItem.delete({
+      where: {
+        id,
+      }
     })
   }
 }
