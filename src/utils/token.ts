166befdd5f06f726f709948a290284d265aad404
omodauda <omodauda.dl@gmail.com>
2022-09-12 09:18:21 +0100

    feat: Prolong token expire time for test purpose

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -5,7 +5,7 @@ import { TokenPayload, TokenData } from '../interfaces/auth.interface';
 export const signToken = (user: User): TokenData => {
   const payload: TokenPayload = { id: user.id };
   const secretKey = process.env.JWT_SECRET;
-  const expiresIn: number = 60 * 60 * 24;
+  const expiresIn: number = 60 * 60 * 240;
 
   const token = jwt.sign(payload, secretKey as jwt.Secret, { expiresIn })
   return { token, expiresIn }
